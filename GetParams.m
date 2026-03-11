90a57fcf970112a9491f4b1b87166fa3aa05bb81
DJHasler <d.j.hasler@student.tudelft.nl>
2026-03-11 13:03:50 +0100

    What in the fuck> What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    		What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>
    What in the fuck>?????????

--- GetParams.m
+++ GetParams.m
@@ -2,11 +2,20 @@ function [heli, atm] = GetParams()
 
 % Helicopter Parameters
 heli.m = 4309;
+heli.payload = 1257;
 heli.R = 14.63/2;
 heli.N = 2;
 heli.RPS = 5.4;
 heli.maxV = 56.67;
+heli.c = 0.53;
+heli.range = 511; %km
+heli.hoverceiling = 1220;
+heli.Ntail = 2;
+heli.Rtail = 2.59/2;
+heli.cgdist = 2.07;
+heli.Iyy = 17259;
 
+heli.sigma = heli.N*heli.c/(pi*heli.R);
 heli.W = heli.m*9.81;
 heli.Omega = heli.RPS * 2*pi;
 heli.A = pi * heli.R^2;
